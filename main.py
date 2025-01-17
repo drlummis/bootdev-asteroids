0d403300a330950b650f8167ed14daacf8635eb1
drlummis <194468973+drlummis@users.noreply.github.com>
2025-01-17 14:11:21 -0500

    Finished Ch2 L2 Game Loop

--- main.py
+++ main.py
@@ -6,9 +6,27 @@ import pygame
 from constants import *
 
 def main():
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Initialize pygame
+    pygame.get_init()
+    
+    # Get a new GUI window
+    screen = pygame.display.set_mode( (SCREEN_WIDTH, SCREEN_HEIGHT) )
+
+    while True:
+        # Event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # Fill screen with solid black
+        screen.fill("black")
+
+        # Refresh the screen
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
